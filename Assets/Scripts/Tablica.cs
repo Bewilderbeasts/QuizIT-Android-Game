daf84ad72304eac5d2e3c4bc2ccae2604c2aa056
marciofolis <65601868+marciofolis@users.noreply.github.com>
2020-11-07 10:51:51 +0100

    tablice
    tablice

--- Assets/Scripts/Tablica.cs
+++ Assets/Scripts/Tablica.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
+
 
 public class Tablica : MonoBehaviour
 {
     private Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    void OnTriggerEnter(Collider other)
+   
+   
+    void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-            anim.SetBool("tablica", true);
+           
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetBool("tablica", true);
+            }
         }
     }
     void OnTriggerExit(Collider other)
@@ -25,6 +35,8 @@ public class Tablica : MonoBehaviour
             anim.SetBool("tablica", false);
         }
     }
+   
+
 
     // Update is called once per frame
     void Update()
