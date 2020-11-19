42706791931da3621642b0113058a01b97a45690
Bewilderbeasts <61983528+Bewilderbeasts@users.noreply.github.com>
2020-11-19 10:55:00 +0100

    Skrypt na znikanie
    Póki co tylko na wynik wiekszy od zera

---
+++ Assets/sciana1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sciana1 : MonoBehaviour
+{
+    public GameObject ScianaDol1;
+    public GameObject ScianaDol2;
+    private int wynik;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wynik = PlayerPrefs.GetInt("Score");
+         if (wynik > 0)
+        {
+
+            ScianaDol1.SetActive(false);
+            ScianaDol2.SetActive(false);
+
+
+        }
+    }
+    
+}
