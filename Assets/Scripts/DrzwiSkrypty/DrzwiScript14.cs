437207bd0c8b8bb2e4709468c01674762d8a1a1c
marciofolis <65601868+marciofolis@users.noreply.github.com>
2020-11-19 12:12:39 +0100

    drzwiskrypty

---
+++ Assets/Scripts/DrzwiSkrypty/DrzwiScript14.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrzwiScript14 : MonoBehaviour
+{
+    private Animator anim;
+    private int currentRound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        currentRound = PlayerPrefs.GetInt("currentRound");
+        if (other.tag == "Player" && currentRound >= 13)
+        {
+            anim.SetBool("DrzwiOpen", true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            anim.SetBool("DrzwiOpen", false);
+        }
+    }
+}
