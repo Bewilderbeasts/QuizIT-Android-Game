5d2fe5e3880e29ea3c47620c49ee7c6402c9802f
Bewilderbeasts <61983528+Bewilderbeasts@users.noreply.github.com>
2020-11-26 23:43:00 +0100

    Przycisk Menu w grze
    Dodano przycisk menu w trakcie gry by moc ja pzerwac w kazdej chwili

---
+++ Assets/menuscript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.CodeDom;
+using System.Globalization;
+using System.Linq;
+
+public class menuscript : MonoBehaviour
+{
+    
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+
+    
+    private int currentRound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataController = FindObjectOfType<DataController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReplayGame()
+    {
+        dataController.ResetCurrentRound();
+        SceneManager.LoadScene("MenuScene");
+    }
+}
